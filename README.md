2a1b6fa0a594d4d1c1210f4d282671d343701b59
Casey Van Groll <caseyvangroll@northwesternmutual.com>
2021-12-02 13:38:30 -0600

    Add readme and lint

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code 2021 Solutions
+## Casey Van Groll
+
+See https://adventofcode.com/
+
+| Day | Challenge   | Solution                  |
+| --- | ----------- | ------------------------- |
+| 1   | Sonar Sweep | [day1.py](./day1/day1.py) |
+| 2   | Dive!       | [day2.py](./day2/day2.py) |
--- day1/day1.py
+++ day1/day1.py
@@ -1,13 +1,13 @@
-def get_depth_increase_ct(inputs):
+def get_depth_increase_ct(depth_measurements):
     depth_increase_ct = 0
-    for i in range(1, len(inputs)):
-        if inputs[i] > inputs[i - 1]:
+    for i in range(1, len(depth_measurements)):
+        if depth_measurements[i] > depth_measurements[i - 1]:
             depth_increase_ct += 1
     return depth_increase_ct
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     with open('./input.txt') as f:
-        inputs = [int(line.strip()) for line in f.readlines()]
-        depth_increase_ct = get_depth_increase_ct(inputs)
-        print("Depth increased {} times.".format(depth_increase_ct))
+        depth_measurements = [int(line.strip()) for line in f.readlines()]
+        depth_increase_ct = get_depth_increase_ct(depth_measurements)
+        print('Depth increased {} times.'.format(depth_increase_ct))
