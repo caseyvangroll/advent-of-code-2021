1d2039f4cb3f99d5493c732ae9283b8fc51e1e46
Casey Van Groll <caseyvangroll@northwesternmutual.com>
2021-12-02 14:01:19 -0600

    Finish day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,13 +1,27 @@
-def get_depth_increase_ct(depth_measurements):
+# Part 1
+def get_depth_increase_ct(depth_measurements, window_size=1):
     depth_increase_ct = 0
-    for i in range(1, len(depth_measurements)):
-        if depth_measurements[i] > depth_measurements[i - 1]:
+    for i in range(window_size, len(depth_measurements)):
+        window_start_idx = i - window_size
+        window_end_idx = i
+
+        cur_window_depth_sum = sum(
+            depth_measurements[window_start_idx: window_end_idx])
+        prev_window_depth_sum = sum(
+            depth_measurements[window_start_idx - 1: window_end_idx - 1])
+
+        if cur_window_depth_sum > prev_window_depth_sum:
             depth_increase_ct += 1
     return depth_increase_ct
+
+# Part 2
 
 
 if __name__ == '__main__':
     with open('./input.txt') as f:
         depth_measurements = [int(line.strip()) for line in f.readlines()]
-        depth_increase_ct = get_depth_increase_ct(depth_measurements)
-        print('Depth increased {} times.'.format(depth_increase_ct))
+        for window_size in [1, 3]:
+            depth_increase_ct = get_depth_increase_ct(
+                depth_measurements, window_size)
+            print('Depth increased {} times with a window size of {}.'.format(
+                depth_increase_ct, window_size))
