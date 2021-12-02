03f27d98a6621744cf2da4e120c7e7dffa4aa8ed
Casey Van Groll <caseyvangroll@northwesternmutual.com>
2021-12-02 14:18:00 -0600

    Finish part 2 of day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,22 +1,28 @@
 def calculate_sub_position(movements):
     horizontal_position = 0
     depth = 0
-    for movement in movements:
-        [direction, magnitude] = movement.split(' ')
+    aim = 0
+
+    for direction, magnitude in movements:
         if direction == 'forward':
-            horizontal_position += int(magnitude)
+            horizontal_position += magnitude
+            depth += magnitude * aim
         elif direction == 'down':
-            depth += int(magnitude)
+            aim += magnitude
         elif direction == 'up':
-            depth -= int(magnitude)
+            aim -= magnitude
 
     return horizontal_position, depth
 
 
 if __name__ == '__main__':
     with open('./input.txt') as f:
-        movements = [line.strip() for line in f.readlines()]
-        horizontal_position, depth = calculate_sub_position(movements)
+        movements = [line.strip().split(' ') for line in f.readlines()]
+        horizontal_position, depth = calculate_sub_position([
+            (direction, int(magnitude))
+            for (direction, magnitude)
+            in movements
+        ])
         print('Final horizontal position: {}'.format(horizontal_position))
         print('Final depth: {}'.format(depth))
         print('Multiplied together: {}'.format(horizontal_position * depth))
