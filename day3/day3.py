a5863376db8ae3c802f7595ce3e5d3733146f588
Casey Van Groll <caseyvangroll@gmail.com>
2021-12-03 13:50:04 -0600

    Continue day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -1,14 +1,12 @@
 from collections import Counter
 
-if True:
-    x = 1
-
-print(x)
-
 
 def analyze_diagnostics(diagnostics):
     gamma_rate = ''
     epsilon_rate = ''
+    life_support_rating = 0
+    oxygen_generator_ratings = [*diagnostics]
+    co2_scrubber_ratings = [*diagnostics]
 
     for column in range(len(diagnostics[0])):
         digits_in_column = [diagnostic[column] for diagnostic in diagnostics]
@@ -20,17 +18,23 @@ def analyze_diagnostics(diagnostics):
         elif '1' not in counter:
             counter['1'] = 0
 
-        most_common, least_common = Counter(digits_in_column).most_common()
-        gamma_rate += most_common[0]
-        epsilon_rate += least_common[0]
+        (most_common_digit, most_common_ct), (least_common_digit, least_common_ct) = Counter(digits_in_column).most_common()
+
+        gamma_rate += most_common_digit
+        epsilon_rate += least_common_digit
+
+        target_oxygen_generator_digit = most_common_digit if most_common_ct != least_common_ct else '1'
+        target_co2_srubber_digit = least_common_digit if most_common_ct != least_common_ct else '0'
+
+        if len(oxygen_generator_ratings) > 1:
+            oxygen_generator_ratings = [
+                diagnostic for diagnostic in oxygen_generator_ratings if diagnostic[column] == target_oxygen_generator_digit]
+        if len(co2_scrubber_ratings) > 1:
+            co2_scrubber_ratings = [
+                diagnostic for diagnostic in co2_scrubber_ratings if diagnostic[column] == target_co2_srubber_digit]
 
     power_consumption = int(gamma_rate, 2) * int(epsilon_rate, 2)
-
-    # Use gamma_rate and epsilon_rate to find
-    oxygen_generator_rating = 0
-    co2_scrubber_rating = 0
-
-    life_support_rating = int(oxygen_generator_rating, 2) * int(co2_scrubber_rating, 2)
+    life_support_rating = int(oxygen_generator_ratings[0], 2) * int(co2_scrubber_ratings[0], 2)
 
     return power_consumption, life_support_rating
 
