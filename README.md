1cf23977fa92cb0f64a9bf8f4a80fa1d15ebd554
Casey Van Groll <caseyvangroll@northwesternmutual.com>
2021-12-06 13:51:09 -0600

    Finish Puzzle 6

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ See https://adventofcode.com/
 | 3   | Binary Diagnostic    | [day3.py](./day3/day3.py) |
 | 4   | Giant Squid          | [day4.py](./day4/day4.py) |
 | 5   | Hydrothermal Venture | [day5.py](./day5/day5.py) |
+| 6   | Lanternfish          | [day6.py](./day6/day6.py) |
--- day6/day6.py
+++ day6/day6.py
@@ -1,8 +1,20 @@
-def do_puzzle():
-    print('TODO')
+def get_fish_ct(current_fish_ages, day_ct):
+    while day_ct > 0:
+        birthing_fish_ct = current_fish_ages.pop(0)
+        current_fish_ages.append(birthing_fish_ct)
+        current_fish_ages[6] += birthing_fish_ct
+        day_ct -= 1
+
+    return sum(current_fish_ages)
 
 
 if __name__ == '__main__':
     with open('./input.txt') as f:
-        input = [line.strip() for line in f.readlines()]
-        do_puzzle()
+        fish_ages = [int(fish_age) for fish_age in f.readline().strip().split(',')]
+        fish_at_each_age = [0] * 9
+        for fish_age in fish_ages:
+            fish_at_each_age[fish_age] += 1
+
+        for day_ct in [80, 256]:
+            fish_ct = get_fish_ct(fish_at_each_age[:], day_ct)
+            print('Fish count after {} days: {}'.format(day_ct, fish_ct))
