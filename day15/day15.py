fdabfc636b23a8b3a3802a329c53e28b16082c97
Casey Van Groll <caseyvangroll@northwesternmutual.com>
2021-12-15 10:11:27 -0600

    Add pseudocode for day 15

--- day15/day15.py
+++ day15/day15.py
@@ -14,3 +14,20 @@ if __name__ == '__main__':
 #   2. only ever explore shortest path(s) in list of paths to explore
 #   3. maintain marker at each cell with whether it's been visited before and the cost of its previous visit (use this to prune paths to explore)
 #   4. if see goal then have found shortest path (because of #2)
+
+# pseudocode:
+
+# paths_to_explore = []
+# shortest_paths_to_each_cell = Infinity for each cell
+# push first 2 paths from upper right into paths_to_explore
+
+# while len(paths_to_explore):
+#   grab shortest_paths_to_explore from paths_to_explore
+#   for each path in shortest_paths_to_explore:
+#     determine next node(s) to explore in path (whichever nodes path hasn't seen AND result in lowest value for path AND haven't been visited by another shorter path)
+#     if next node(s) includes end node stop and return this path
+#     next_paths = this path + next node(s)
+#     for next_path in next_paths:
+#       if ending in a node that hasn't been visited before or has only been visited by a longer path:
+#         update shortest_paths_to_each_cell to contain this path's cost
+#     drop next_pathsinto paths_to_explore along with their corresponding costs
